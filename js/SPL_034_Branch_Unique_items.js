40ae051fb02cd9bb50b1f4729580dda86319eb1e
Sean Girard <seangirard@yahoo.com>
2015-11-05 15:23:18 -0800

    ajax

--- js/SPL_034_Branch_Unique_items.js
+++ js/SPL_034_Branch_Unique_items.js
@@ -2,7 +2,7 @@ $.ajax({
 	url: '/wordpress/wp-admin/admin-ajax.php'
 	, data: { action: 'spl_reports' }
 			//, type: 'POST'
-			, params: { spl-reports:'test' }
+			, params: { 'spl-reports':'test' }
 	})
 	.done(function(obj) {  
 		console.log(obj);
