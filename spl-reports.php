2360afb17538cb8f955206c7f8f5c3a082391719
Sean Girard <seangirard@yahoo.com>
2015-11-05 14:40:19 -0800

    ajax

--- spl-reports.php
+++ spl-reports.php
@@ -19,11 +19,12 @@ if ( !isset($_SESSION) ) {
 }
 
 if ( is_admin() ) {
-		    add_action( 'wp_ajax_my_frontend_action', 'my_frontend_action_callback' );
-		    add_action( 'wp_ajax_nopriv_my_frontend_action', 'my_frontend_action_callback' );
-		    //add_action( 'wp_ajax_my_backend_action', 'my_backend_action_callback' );
-		    // Add other back-end action hooks here
-			}
+  add_action( 'wp_ajax_my_frontend_action', 'my_frontend_action_callback' );
+  add_action( 'wp_ajax_nopriv_my_frontend_action', 'my_frontend_action_callback' );
+  //add_action( 'wp_ajax_my_backend_action', 'my_backend_action_callback' );
+  // Add other back-end action hooks here
+  exit;
+}
 
 
 // remember to visit permalinks page to flush cache
@@ -68,17 +69,7 @@ function wp_spl_reports($params) {
 	if ( !empty($view) ) {
   	$view = explode('/', $view);
     if ( !empty($view) ) {
-    	if ( is_admin() ) {
-		    add_action( 'wp_ajax_my_frontend_action', 'my_frontend_action_callback' );
-		    add_action( 'wp_ajax_nopriv_my_frontend_action', 'my_frontend_action_callback' );
-		    //add_action( 'wp_ajax_my_backend_action', 'my_backend_action_callback' );
-		    // Add other back-end action hooks here
-			} else {
-			    // Add non-Ajax front-end action hooks here
-					//wp_send_json( array('test'=>'ing') );
-					$report = new SPL_Report($view, $params);
-			}
-
+    	$report = new SPL_Report($view, $params);
 		}
 	}
 
