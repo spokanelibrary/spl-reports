543e2dbde2652a8659de31093c07b161f2687248
Sean Girard <seangirard@yahoo.com>
2015-11-05 09:16:22 -0800

    boilerplate

---
+++ class/SPL_Report.php
@@ -0,0 +1,86 @@
+<?php
+
+class SPL_Report {
+
+	var $params;
+	var $output;
+
+	var $apikey;
+	
+	var $summary = null;
+
+	function __construct() {
+		
+		$this->apikey = getenv('SPL_KEY');
+
+	}
+
+	public function output() {
+		return $this->output;
+	}
+
+	public static function curlProxy($url, $params, $method='post', $auth=null) {
+	  $result = new stdClass();
+	  $result->response = false;
+
+	  // create a new cURL resource
+	  $ch = curl_init();
+
+	  if ( 'post' == $method ) {
+	    // setup for an http post
+	    curl_setopt($ch, CURLOPT_POST, 1);
+	    // 'cause cURL doesn't like multi-dimensional arrays
+	    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
+	  } elseif ( 'get' == $method ) {
+	    if ( is_array($params) ) {
+	      $url .= '?' . http_build_query($params);
+	    }
+	  } elseif ( 'delete' == $method ) {
+	    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
+	  } elseif ( 'put' == $method ) {
+	    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+	    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
+	  }
+
+
+	  // set URL and other appropriate options
+	  curl_setopt($ch, CURLOPT_URL, $url);
+	  //curl_setopt($ch, CURLOPT_HEADER, false);
+
+	  // follow redirects
+	  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+
+	  // set auth params
+	  if ( is_array($auth) ) {
+	    //curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_NTLM);  
+	    curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC); // CURLAUTH_ANY
+	    curl_setopt($ch, CURLOPT_USERPWD, $auth['user'] . ':' . $auth['pass']);
+	  }
+
+	  // set returntransfer to true to prevent browser echo
+	  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+	  $ua = $_SERVER['HTTP_USER_AGENT']; // optional
+	  if (isset($ua)) {
+	      curl_setopt($ch, CURLOPT_USERAGENT, $ua);
+	  }
+
+	  // grab URL
+	  $result->response = curl_exec($ch);
+
+	  // grab http response code
+	  $result->httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+	   
+	  // close cURL resource, and free up system resources
+	  curl_close($ch);
+
+	  return $result;
+	}
+
+}
+
+
+
+
+
+?>
---
+++ spl-reports.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * @package SPL_Reports
+ * @version 0.1
+ */
+
+/*
+Plugin Name: SPL Reports
+Plugin URI: http://www.spokanelibrary.org/
+Description: Hooks the <code>[spl_reports]</code> shortcode to show a variety of reports.
+Author: Sean Girard
+Author URI: http://seangirard.com
+Version: 0.1
+*/
+
+if ( !isset($_SESSION) ) {
+	session_start();
+}
+
+// remember to visit permalinks page to flush cache
+function add_spl_reports_rewrite_rules() {   
+
+	// rpa pages
+	$url = 'reports';
+	$page = 'reports';
+	$query = 'spl-reports'; // you must register in query vars
+	// grab the entire trailing string
+  add_rewrite_rule(  
+      $url."/(.*)?$",
+      'index.php?pagename='.$page.'&'.$query.'=$matches[1]',  
+      'top');
+
+}
+add_action( 'init', 'add_spl_reports_rewrite_rules' );
+
+function add_spl_reports_query_vars($vars) {
+    $spl_vars = array(
+  			'spl-reports'
+    );
+    return array_merge($spl_vars, $vars);
+}
+add_filter('query_vars', 'add_spl_reports_query_vars');
+
+require_once 'class/SPL_Report.php';
+
+
+function wp_spl_reports($params) {
+
+	$report = null;
+
+	switch ( $params[0] ) {
+
+		default:
+			break;
+	}
+
+	
+	if ( is_object($report) ) {
+		return $report->output();
+	}
+
+}
+
+add_shortcode('spl_reports', 'wp_spl_reports');
+
+?>
