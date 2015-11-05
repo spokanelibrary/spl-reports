6bc01b2e5fe00d1d45d5642d2fd197c0dd5f906a
Sean Girard <seangirard@yahoo.com>
2015-11-05 15:56:41 -0800

    json

--- spl-reports.php
+++ spl-reports.php
@@ -70,12 +70,14 @@ function spl_reports($view=null, $params=null) {
 }
 
 function spl_reports_ajax() {
-	wp_send_json( spl_reports($_POST) );
+	wp_send_json($_POST);
+	//wp_send_json( spl_reports($_POST) );
 	wp_die();
 } 
 
 function spl_reports_ajax_anon() {
-	wp_send_json( spl_reports($_POST) );
+	wp_send_json($_POST);
+	//wp_send_json( spl_reports($_POST) );
 	wp_die();
 } 
 
