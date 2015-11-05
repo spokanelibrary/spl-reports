ea28591932db02fedc0237e2b5251ddb037b932c
Sean Girard <seangirard@yahoo.com>
2015-11-05 15:10:24 -0800

    ajax

--- spl-reports.php
+++ spl-reports.php
@@ -14,14 +14,14 @@ Author URI: http://seangirard.com
 Version: 0.1
 */
 
-/*
+
 if ( is_admin() ) {
   add_action( 'wp_ajax_spl_reports', 'spl_reports_ajax' );
   add_action( 'wp_ajax_nopriv_spl_reports', 'spl_reports_ajax_anon' );
   //add_action( 'wp_ajax_my_backend_action', 'my_backend_action_callback' );
   // Add other back-end action hooks here
 } 
-*/
+
 
 // remember to visit permalinks page to flush cache
 function add_spl_reports_rewrite_rules() {   
@@ -47,15 +47,17 @@ function add_spl_reports_query_vars($vars) {
 }
 add_filter('query_vars', 'add_spl_reports_query_vars');
 
-require_once 'class/SPL_Report.php';
 
 function wp_spl_reports($params=null) {
 
 	return spl_reports($params);
 
 }
+add_shortcode('spl_reports', 'wp_spl_reports');
 
 function spl_reports($params=null, $ajax=null) {
+	require_once 'class/SPL_Report.php';
+	
 	$report = null;
 
 	$view = get_query_var('spl-reports');
@@ -78,15 +80,13 @@ function spl_reports($params=null, $ajax=null) {
 	
 }
 
-/*
+
 function spl_reports_ajax() {
-	spl_reports(null, true)
+	spl_reports(null, true);
 } 
 function spl_reports_ajax_anon() {
-	spl_reports(null, true)
+	spl_reports(null, true);
 } 
-*/
 
-add_shortcode('spl_reports', 'wp_spl_reports');
 
 ?>
