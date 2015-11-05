fff5fa2e5b43a74f23ddbf3f1b7bf264c271a5ac
Sean Girard <seangirard@yahoo.com>
2015-11-05 12:43:08 -0800

    js

--- class/SPL_034_Branch_Unique_items.php
+++ class/SPL_034_Branch_Unique_items.php
@@ -5,7 +5,10 @@ class SPL_034_Branch_Unique_items extends SPL_Report {
 	var $data;
 
 	function __construct() {
-		$this->data = 'testing some data' .  plugins_url('js', dirname(__FILE__));
+		$js = plugins_url('js/', dirname(__FILE__));
+		$filename = $js.get_class();
+		
+		$this->data = $filename;
 	}
 
 }
