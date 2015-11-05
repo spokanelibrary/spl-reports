f879cdc6aa14c581320c3ba5b4576e5d926756e9
Sean Girard <seangirard@yahoo.com>
2015-11-05 12:40:28 -0800

    path

--- class/SPL_034_Branch_Unique_items.php
+++ class/SPL_034_Branch_Unique_items.php
@@ -5,7 +5,7 @@ class SPL_034_Branch_Unique_items extends SPL_Report {
 	var $data;
 
 	function __construct() {
-		$this->data = 'testing some data' .  plugins_url(null, 'spl-reports');
+		$this->data = 'testing some data' .  plugins_url(null, dirname(__FILE__));
 	}
 
 }
