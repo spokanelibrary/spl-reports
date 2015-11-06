b7f66ef6a36721dfc9e8b40a60dda218c9046c1b
Sean Girard <seangirard@yahoo.com>
2015-11-06 11:51:56 -0800

    js

--- js/SPL_Reports.js
+++ js/SPL_Reports.js
@@ -1,15 +1,13 @@
 var splReportConfig = {
 
-	api: { 
-
-		{ 
-			url: '/wordpress/wp-admin/admin-ajax.php'
-		, data: { action: 'spl_reports'
-						, params: { ajax:true, id: $('.spl-report').data('spl-report-id'} 
-						}
-				, type: 'POST'
-				
-		}
+	api: {  
+		url: '/wordpress/wp-admin/admin-ajax.php'
+	, data: { action: 'spl_reports'
+					, params: { ajax:true, id: $('.spl-report').data('spl-report-id'} 
+					}
+			, type: 'POST'
+			
+	}
 
 
 }
