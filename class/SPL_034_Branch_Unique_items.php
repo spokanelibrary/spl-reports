3f41150eadc8fbe99076d08a6a9ab87cee8801bf
Sean Girard <seangirard@yahoo.com>
2015-11-06 08:11:35 -0800

    config

--- class/SPL_034_Branch_Unique_items.php
+++ class/SPL_034_Branch_Unique_items.php
@@ -4,13 +4,14 @@ class SPL_034_Branch_Unique_items extends SPL_Report {
 
 	
 	function __construct($parent) {
+		$this->parent = $parent;
 		//$this->params = $params;
 		//$this->config = $config;
 	}
 
 	public function getReportData() {
 		//return 'test';
-		return $parent->$params;
+		return $this->parent->$params;
 	}
 
 }
