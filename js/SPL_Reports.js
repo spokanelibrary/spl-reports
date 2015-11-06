20c93380d59a3977a5d360488cffa2bea0d6ed00
Sean Girard <seangirard@yahoo.com>
2015-11-06 11:46:34 -0800

    js

--- js/SPL_Reports.js
+++ js/SPL_Reports.js
@@ -2,7 +2,7 @@ var splReportConfig = {
 
 	api: { 
 		endpoint: '/wordpress/wp-admin/admin-ajax.php' 
-	,	test: $('.spl-report').data('spl-report-id');
+	,	test: $('.spl-report').data('spl-report-id')
 	}
 
 
