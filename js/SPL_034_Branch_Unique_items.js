9857954d37accd70a00d1f291f62517b94d6fce9
Sean Girard <seangirard@yahoo.com>
2015-11-06 12:39:38 -0800

    js

--- js/SPL_034_Branch_Unique_items.js
+++ js/SPL_034_Branch_Unique_items.js
@@ -2,10 +2,11 @@ var splReport = {
 	config:splReportConfig
 
 	,init: function() {
-		_self = this;
+		
 		this.initUI();
 	} 
 	,initUI: function() {
+		_self = this;
 		$('body').on('submit', '.spl-report-control', function(e) {
 			e.preventDefault();
 			_self.getReport();
