3730abd589c48fe3ceeb530f3153c2d2fb4aabbd
Sean Girard <seangirard@yahoo.com>
2015-11-09 11:15:35 -0800

    nonce

--- class/SPL_Report.php
+++ class/SPL_Report.php
@@ -21,8 +21,8 @@ class SPL_Report {
 		$report = new $class->name($this->params, $this->config);
 
 		if ( $this->params['ajax'] ) {
-			
-			$this->output = wp_verify_nonce( $_REQUEST['security'] );
+
+			$this->output = wp_verify_nonce( $_POST['security'], 'spl-report-nonce-'.$this->params['id'] );
 			//$this->output = $_REQUEST['security'];
 			//$this->output = 'spl-report-nonce-'.$this->params['id'];
 			//$this->output = $report->processData($report->getReportData());
