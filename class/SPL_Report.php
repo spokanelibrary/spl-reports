405b3efe8c2d5b10bf1bebd73785f3ba736fde07
Sean Girard <seangirard@yahoo.com>
2015-11-09 10:36:38 -0800

    nonce

--- class/SPL_Report.php
+++ class/SPL_Report.php
@@ -21,7 +21,8 @@ class SPL_Report {
 		$report = new $class->name($this->params, $this->config);
 
 		if ( $this->params['ajax'] ) {
-			$this->output = $report->processData($report->getReportData());
+			$this->output = check_ajax_referer( 'spl-report-nonce-'.$post->ID );
+			//$this->output = $report->processData($report->getReportData());
 		} else {
 			$report->loadJs();
 			$html = '<div class="spl-report"
