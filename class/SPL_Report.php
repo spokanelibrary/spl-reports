53caa3411883769820d11e7b7d7c5edadfa1ca4f
Sean Girard <seangirard@yahoo.com>
2015-11-09 11:11:00 -0800

    nonce

--- class/SPL_Report.php
+++ class/SPL_Report.php
@@ -21,7 +21,8 @@ class SPL_Report {
 		$report = new $class->name($this->params, $this->config);
 
 		if ( $this->params['ajax'] ) {
-			$this->output = wp_verify_nonce( 'spl-report-nonce-'.$this->params['id'], $_REQUEST['security'] );
+			wp_create_nonce( 'spl-report-nonce-'.$this->params['id'] );
+			$this->output = wp_verify_nonce( $_REQUEST['security'], 'spl-report-nonce-'.$this->params['id'] );
 			//$this->output = $_REQUEST['security'];
 			//$this->output = 'spl-report-nonce-'.$this->params['id'];
 			//$this->output = $report->processData($report->getReportData());
