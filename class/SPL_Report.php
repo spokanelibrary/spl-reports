75027c842e72e8a658e64a52b4c9478006471037
Sean Girard <seangirard@yahoo.com>
2015-11-09 11:01:21 -0800

    nonce

--- class/SPL_Report.php
+++ class/SPL_Report.php
@@ -22,7 +22,7 @@ class SPL_Report {
 
 		if ( $this->params['ajax'] ) {
 			$this->output = wp_verify_nonce( $_REQUEST['security'], 'spl-report-nonce-'.$this->params['id'] );
-			//$this->output = 'spl-report-nonce-'.$post->ID;
+			$this->output = 'spl-report-nonce-'.$this->params['id'];
 			//$this->output = $report->processData($report->getReportData());
 		} else {
 			$report->loadJs();
