44f027e76b8adcaffc3e99c51d350cc0602110d9
Sean Girard <seangirard@yahoo.com>
2015-11-11 07:59:46 -0800

    nav

--- class/SPL_Report.php
+++ class/SPL_Report.php
@@ -138,9 +138,6 @@ class SPL_Report {
 					foreach ( $menu->list as $i=>$item ) {
 						if ( $item['divider'] ) {
 							//$html .= '<li role="separator" class="divider"></li>';
-							$html .= '</ul>';
-							$html .= '<hr>';
-							$html .= '<ul class="nav nav-pills nav-stacked">';
 						} else {
 							$html .= '<li><a href="'.$item[1].'">'.$item[0].'</a></li>';
 						}
