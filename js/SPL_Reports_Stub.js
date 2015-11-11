f183064c15d5ed7836d22bc725d7c3e5949046fb
Sean Girard <seangirard@yahoo.com>
2015-11-11 15:51:08 -0800

    147

--- js/SPL_Reports_Stub.js
+++ js/SPL_Reports_Stub.js
@@ -1,7 +1,14 @@
+/*
+ *	Rename file to match api/html 
+ *	this file will be autoloaded
+ *	and setUi() autorun
+ *
+ *	Used for report-specific mods
+ */
 var splReportUI = {
 
 	setUI: function() {
-		console.log('stub');
+		//console.log('stub');
 	}
 
 }.setUI();
