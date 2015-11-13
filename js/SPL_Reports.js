e3f18ea4c74ca7ff0f36564988dae6b8c53243ec
Sean Girard <seangirard@yahoo.com>
2015-11-13 07:12:54 -0800

    date

--- js/SPL_Reports.js
+++ js/SPL_Reports.js
@@ -24,7 +24,7 @@ var splReport = {
 		});
 
 
-		$datepicker = $('.date');
+		$datepicker = $('.spl-report-date');
 		if ( $datepicker.length ) {
 			$datepicker.datepicker({
 	        startDate: "+1d",
