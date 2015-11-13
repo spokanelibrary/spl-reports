e809aadbdc03989e4bce616adfd8e28e349ae52f
Sean Girard <seangirard@yahoo.com>
2015-11-13 15:04:09 -0800

    class

--- class/SPL_Report.php
+++ class/SPL_Report.php
@@ -26,7 +26,7 @@ class SPL_Report {
 				include $class->path;
 				if ( class_exists($class->name) ) {
 					//$report = new $class->name($this->params, $this->config);
-					$report = $this;
+					$report = new SPL_Report($this->params, $this->config);
 					if ( $this->params['ajax'] ) {
 						if ( wp_verify_nonce( $_REQUEST['nonce'], 'spl-report-nonce-'.$this->params['id'] ) ) {
 							$this->output = $report->processData($report->getReportData());
