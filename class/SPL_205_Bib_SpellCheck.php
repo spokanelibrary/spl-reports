45af37c9d401cadbc9242de0de7ed4b2690ca0aa
Sean Girard <seangirard@yahoo.com>
2016-02-29 15:29:09 -0800

    spell

---
+++ class/SPL_205_Bib_SpellCheck.php
@@ -0,0 +1,19 @@
+<?php
+
+class SPL_202_CHOF_Nominate extends SPL_Report {
+
+	/*
+	 *	overload api endpoint, if necessary
+	 */
+	//var $api = '';
+
+	/*
+	 *	overload data processing, if necessary
+	 */
+	protected function processData($data=null) {	
+		return parent::processData($data);
+	}
+
+}
+
+?>
---
+++ js/SPL_205_Bib_SpellCheck.js
@@ -0,0 +1,16 @@
+/*
+ *	Rename file to match api/html 
+ *	this file will be autoloaded
+ *	and setUi() autorun
+ *
+ *	Used for report-specific mods
+ */
+var splReportUI = {
+
+	
+
+	setUI: function() {
+		console.log('retreiving char sets');
+	}
+
+}.setUI();
