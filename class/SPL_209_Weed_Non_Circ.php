269e4f2f5fe824a9787cc23b7daf6b2e265c8750
Sean Girard <seangirard@yahoo.com>
2016-05-27 14:03:14 -0700

    weed

---
+++ class/SPL_209_Weed_Non_Circ.php
@@ -0,0 +1,19 @@
+<?php
+
+class SPL_209_Weed_Non_Circ extends SPL_Report {
+
+	/*
+	 *	overload api endpoint, if necessary
+	 */
+	//var $api = '';
+
+	/*
+	 *	overload data processing, if necessary
+	 */
+	protected function processData($data=null) {	
+		return parent::processData($data);
+	}
+
+}
+
+?>
