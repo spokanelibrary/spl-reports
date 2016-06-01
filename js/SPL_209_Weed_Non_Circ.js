489c45504df2b9252e22340e792f17a31b3aebb7
Sean Girard <seangirard@yahoo.com>
2016-06-01 15:01:10 -0700

    controls

--- js/SPL_209_Weed_Non_Circ.js
+++ js/SPL_209_Weed_Non_Circ.js
@@ -23,7 +23,8 @@ var splReportUI = {
 			if ( obj.controls ) {
 				console.log(obj.controls);
 				tmpl = Handlebars.compile( $('.spl-report-control-tmpl').html() );
-				$('.spl-report-control').html( tmpl({ controls:obj.controls }));
+				//$('.spl-report-control').html( tmpl({ controls:obj.controls }));
+				$('.spl-report-control').html('my controls');
 			}
 			/*
 
