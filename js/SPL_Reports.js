f231cab2a0e2a01b9b5f920a4cdbd677faee748f
Sean Girard <seangirard@yahoo.com>
2016-09-23 08:40:34 -0700

    validate.js

--- js/SPL_Reports.js
+++ js/SPL_Reports.js
@@ -16,6 +16,25 @@ var splReport = {
 	,init: function() {
 		_this = this;
 		this.initUI();
+
+		$('form').validate({
+	        highlight: function(element) {
+	            $(element).closest('.form-group').addClass('has-error');
+	        },
+	        unhighlight: function(element) {
+	            $(element).closest('.form-group').removeClass('has-error');
+	        },
+	        errorElement: 'span',
+	        errorClass: 'help-block',
+	        errorPlacement: function(error, element) {
+	            if(element.parent('.input-group').length) {
+	                error.insertAfter(element.parent());
+	            } else {
+	                error.insertAfter(element);
+	            }
+	        }
+	    });
+			
 	}
 	,initUI: function() {
 		$('body').on('submit', '.spl-report-control', function(e) {
